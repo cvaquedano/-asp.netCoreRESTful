9b6833a9f4b36b69b6ac21caf2ded4c8c55219a0
Carlos Vaquedano <carlos.vaquedano@acklenavenue.com>
2020-10-11 17:06:13 -0600

    add delete option for a single resourse and his childrens

--- CourseLibrery.API/Controllers/AuthorsController.cs
+++ CourseLibrery.API/Controllers/AuthorsController.cs
@@ -69,5 +69,25 @@ namespace CourseLibrery.API.Controllers
             Response.Headers.Add("Allow", "GET,OPTIONS,POST");
             return Ok();
         }
+
+        [HttpDelete("{authorId}")]
+
+        public ActionResult DeleteAuthor(Guid authorId)
+        {
+            
+
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+
+            }
+
+            _courseLibraryRepository.DeleteAuthor(authorFromRepo);
+            _courseLibraryRepository.Save();
+
+            return NoContent();
+
+        }
     }
 }
